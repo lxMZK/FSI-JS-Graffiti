39090d163d7989f76b936d82ce90af5b49d46384
Alex Mizak <alexmmizak@gmail.com>
2022-03-29 18:38:10 -0400

    Graffiti Alex Mizak submission

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let header = document.querySelector('#page-header')
+header.style.textAlign = 'left'
 
-// Select some elements...
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+    dogImages[i].style.borderRadius = '50px'
+    dogImages[i].style.transform.scaleX(-1)
+}
+
+let dogNames = document.querySelectorAll('.dog-name')
+for(let i = 0; i < dogNames.length; i++){
+    dogNames[i].style.textAlign = 'left'
+}
+
+let footer = document.querySelector('.footer')
+footer.style.color = 'blue'
